334c0ef873d8131ef3045e0ddf54e7a64e435453
Nikhil Singh <95328684+nikhil-singh1@users.noreply.github.com>
2025-09-23 19:41:08 +0530

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -133,20 +133,22 @@ const ContactPage = () => {
       </section>
 
       {/* Map Section */}
-      <section className="w-full h-[50vh]">
-    <iframe
+    <section className="w-full h-[50vh]">
+  <iframe
     title="Our Office Location"
-    src="https://maps.google.com/maps?q=Office%20120.3%20Regus,%20Cardinal%20Point,%20Park%20Road,%20Rickmansworth,%20WD3%201RE&output=embed"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2474.65178192738!2d-0.4769!3d51.6382!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48766d79d47eab3d%3A0xf2e42b64e55c1ed!2sRegus%20-%20Rickmansworth%2C%20Cardinal%20Point!5e0!3m2!1sen!2suk!4v1695483112345!5m2!1sen!2suk"
     width="100%"
     height="100%"
     style={{ border: 0 }}
     allowFullScreen
     loading="lazy"
-     ></iframe>
-     </section>
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</section>
     </>
   );
 };
 
 export default ContactPage;
+
 
